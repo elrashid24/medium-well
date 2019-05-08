4320d86e625fda23661b6b171117e9b68d58a23b
Elrashid_Elzein <elrashidelzein8@gmail.com>
2019-05-07 18:09:02 -0700

    Fix error reducer naming issue

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 import entities from './entities_reducer';
 import session from './session_reducer';
 import ui from './ui_reducer';
-import errors from './errors_reducer';
+import errors from './errors_reducer.js';
 
 const rootReducer = combineReducers({
     entities,
