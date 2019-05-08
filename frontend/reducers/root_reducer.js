f6861cc3bebe79ffa864e7252b17509dd5e9ce4e
Elrashid_Elzein <elrashidelzein8@gmail.com>
2019-05-07 17:52:54 -0700

    Recreate errors_reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,9 +2,8 @@ import { combineReducers } from 'redux';
 
 import entities from './entities_reducer';
 import session from './session_reducer';
-import errors from './errors_reducer';
 import ui from './ui_reducer';
-
+import errors from './errors_reducer';
 
 const rootReducer = combineReducers({
     entities,
